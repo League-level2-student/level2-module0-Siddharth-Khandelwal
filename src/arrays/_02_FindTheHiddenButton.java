77f2387ca9c75a19f4e0936c1191e346c7bf32b2
league <league@192.168.1.200>
2024-03-30 16:29:08 -0700

    robot race done, step 8 and below done on find the hidden button

--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -20,9 +20,9 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	JPanel panel;
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	JButton[] array;
 	//2 create an int variable called hiddenButton
-	
+	int hiddenButton = 0;
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
@@ -33,10 +33,16 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		
+		String inputted = JOptionPane.showInputDialog("Enter a Number (Positive Only");
+		int num = Integer.parseInt(inputted);
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
+		array = new JButton[num];
 		//5. Make a for loop to iterate through the JButton array
+		for (int i = 0; i < array.length; i++) {
+			array[i]= new JButton();
+			array[num].addActionListener(this);
+			panel.add(array[num]);
+		}
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
 			//8. add each JButton to the panel
