192f72791745a104316ad62e581f5b9f08fbda7a
league <league@192.168.1.200>
2024-03-30 16:22:43 -0700

    Robot race done, step 8 and below done of find the hidden button

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -2,6 +2,8 @@ package arrays;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
@@ -26,6 +28,7 @@ public class _01_RobotRace {
 		//5. use another for loop to iterate through the array and make each robot move 
 		//   a random amount less than 50.
 		boolean raceprogress = true;
+		int winner = 0;
 		while (raceprogress) {
 
 
@@ -34,6 +37,7 @@ public class _01_RobotRace {
 				int g = rand.nextInt(51);
 				array[i].move(g);
 				if(array[i].getY()<0) {
+					winner = i;
 					raceprogress = false;
 					break;
 				}
@@ -43,8 +47,18 @@ public class _01_RobotRace {
 
 
 
-		//7. declare that robot the winner and throw it a party!
-
+		//7. declare that robot the winner and throw it a party
+		JOptionPane.showMessageDialog(null, "Robot "+winner+" is the winner");
+		array[winner].setY(300);
+		array[winner].setX(400);
+		array[winner].sparkle();
+		
+			
+		for (int i = 0; i < array.length; i++) {
+			if(i!=winner){
+				array[i].hide();
+			}
+		}
 		//8. try different races with different amounts of robots.
 
 		//9. make the robots race around a circular track.
